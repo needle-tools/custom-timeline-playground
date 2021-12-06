1dddf894d6202b8fae288bddc4b89701777e620c
Felix Herbst <felix.herbst@gmail.com>
2021-12-06 17:06:17 +0100

    move gltf clip/track to UnityGLTF package

--- extras/Runtime/Export/Timeline/GltfRecording.cs
+++ extras/Runtime/Export/Timeline/GltfRecording.cs
@@ -1,55 +1,54 @@
 using UnityEngine;
 
-[ExecuteAlways]
-public class GltfRecording : MonoBehaviour
+namespace UnityGLTF.Timeline
 {
-	public Transform root;
-	
-	private void OnEnable()
+	[ExecuteAlways]
+	public class GltfRecording : MonoBehaviour
 	{
-		if(Application.isPlaying)
+		public Transform root;
+
+		private void OnEnable()
 		{
-			Time.captureFramerate = 60;
-			StartNow();
+			if (Application.isPlaying)
+			{
+				Time.captureFramerate = 60;
+				StartNow();
+			}
+		}
+
+		private void OnDisable()
+		{
+			if (Application.isPlaying)
+			{
+				Time.captureFramerate = 0;
+				StopNow();
+			}
+		}
+
+		private GltfRecorder recorder = null;
+
+		[ContextMenu("Start Recording")]
+		void StartNow()
+		{
+			recorder = new GltfRecorder(root);
+			recorder.StartRecording(Time.realtimeSinceStartupAsDouble);
+			recording = true;
+		}
+
+		[ContextMenu("Stop Recording")]
+		void StopNow()
+		{
+			recording = false;
+			recorder.EndRecording("Assets/Recording.glb");
+		}
+
+		private bool recording = false;
+
+		private void LateUpdate()
+		{
+			if (!recording) return;
+
+			recorder.UpdateRecording(Time.realtimeSinceStartupAsDouble);
 		}
 	}
-
-	private void OnDisable()
-	{
-		if (Application.isPlaying)
-		{
-			Time.captureFramerate = 0;
-			StopNow();
-		}
-	}
-
-	private GltfRecorder recorder = null;
-	
-	[ContextMenu("Start Recording")]
-	void StartNow()
-	{
-		recorder = new GltfRecorder(root);
-		recorder.StartRecording(Time.realtimeSinceStartupAsDouble);
-		recording = true;
-	}
-
-	[ContextMenu("Stop Recording")]
-	void StopNow()
-	{
-		recording = false;
-		recorder.EndRecording("Assets/Recording.glb");
-	}
-
-	private bool recording = false;
-
-	private void LateUpdate()
-	{
-		if (!recording) return;
-
-		recorder.UpdateRecording(Time.realtimeSinceStartupAsDouble);
-		// Size = recorder.data.Sum(x => x.Value.Size + 4 + 4);
-	}
-
-	// public int Size;
-
 }
